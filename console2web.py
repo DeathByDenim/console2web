25cb641d29cbd0632ede81572d95bcd485cc0e1f
DeathByDenim <jarno@jarno.ca>
2022-01-29 20:59:48 -0500

    Fix global var for process

--- console2web.py
+++ console2web.py
@@ -9,8 +9,6 @@ import sys
 HOST = '127.0.0.1'
 PORT = 8080
 
-process = None
-
 async def websocket_handler(request):
     print('Websocket connection starting', flush=True)
     ws = web.WebSocketResponse()
@@ -20,25 +18,15 @@ async def websocket_handler(request):
 
     try:
         async for msg in ws:
-            print("Message: ")
-            print(msg, flush=True)
             if msg.type == WSMsgType.TEXT:
-                print("Message data: " + msg.data, flush=True)
                 if msg.data == 'close':
                     await ws.close()
                 else:
-                    print("Message data not close: " + msg.data + '\n', flush=True)
-                    print(process)
-                    if process != None:
-                        process.stdin.write((msg.data + '\n').encode('utf-8'))
+                    if app['mutable_state']['process'] != None:
+                        app['mutable_state']['process'].stdin.write((msg.data + '\n').encode('utf-8'))
                     else:
                         print("Process was None")
-                    # if request.app['process'] != None:
-                    #     request.app['process'].stdin.write((msg.data + '\n').encode('utf-8'))
-
-                    # await ws.send_str(msg.data + '/answer')
     finally:
-        print("Discarding", flush=True)
         request.app['websockets'].discard(ws)
 
     print('Websocket connection closed', flush=True)
@@ -48,18 +36,16 @@ async def listen_to_redis(app):
     await asyncio.sleep(10)
 
 async def system_process(app):
-    # app['process'] = await asyncio.create_subprocess_exec(
     process = await asyncio.create_subprocess_exec(
         app['command'],
         *app['args'],
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE)
+    app['mutable_state']['process'] = process
     while True:
-        print(app['system_process'])
         data = await process.stdout.readline()
-        # data = await app['process'].stdout.readline()
         line = data.decode('ascii').rstrip()
-        print("Line: " + line, flush=True)
+        print(line, flush=True)
         for ws in set(app['websockets']):
             await ws.send_str(line)
 
@@ -100,6 +86,7 @@ if __name__ == "__main__":
     app['command'] = command
     app['args'] = args
     app['websockets'] = weakref.WeakSet()
+    app['mutable_state'] = {"process": None}
     app.on_startup.append(start_background_tasks)
     app.on_cleanup.append(cleanup_background_tasks)
     app.on_shutdown.append(cancel_background_tasks)

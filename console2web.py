e48b2f3fa8a8efe6142fee2a1cd604336d69dcd7
DeathByDenim <jarno@jarno.ca>
2022-01-29 20:35:01 -0500

    Stop getting stuck in an infinite loop on shutdown

--- console2web.py
+++ console2web.py
@@ -9,46 +9,57 @@ import sys
 HOST = '127.0.0.1'
 PORT = 8080
 
+process = None
+
 async def websocket_handler(request):
-    print('Websocket connection starting')
+    print('Websocket connection starting', flush=True)
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-    print('Websocket connection ready')
+    print('Websocket connection ready', flush=True)
     request.app['websockets'].add(ws)
 
     try:
         async for msg in ws:
-            print(msg)
+            print("Message: ")
+            print(msg, flush=True)
             if msg.type == WSMsgType.TEXT:
-                print(msg.data)
+                print("Message data: " + msg.data, flush=True)
                 if msg.data == 'close':
                     await ws.close()
                 else:
-                    print(msg.data + '\n')
-                    if request.app['process'] != None:
-                        request.app['process'].stdin.write((msg.data + '\n').encode('utf-8'))
+                    print("Message data not close: " + msg.data + '\n', flush=True)
+                    print(process)
+                    if process != None:
+                        process.stdin.write((msg.data + '\n').encode('utf-8'))
+                    else:
+                        print("Process was None")
+                    # if request.app['process'] != None:
+                    #     request.app['process'].stdin.write((msg.data + '\n').encode('utf-8'))
 
                     # await ws.send_str(msg.data + '/answer')
     finally:
+        print("Discarding", flush=True)
         request.app['websockets'].discard(ws)
 
-    print('Websocket connection closed')
+    print('Websocket connection closed', flush=True)
     return ws
 
 async def listen_to_redis(app):
     await asyncio.sleep(10)
 
 async def system_process(app):
-    app['process'] = await asyncio.create_subprocess_exec(
+    # app['process'] = await asyncio.create_subprocess_exec(
+    process = await asyncio.create_subprocess_exec(
         app['command'],
         *app['args'],
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE)
     while True:
-        # proc.stdin.write(b'hello\n')
-        data = await app['process'].stdout.readline()
+        print(app['system_process'])
+        data = await process.stdout.readline()
+        # data = await app['process'].stdout.readline()
         line = data.decode('ascii').rstrip()
-        print(line)
+        print("Line: " + line, flush=True)
         for ws in set(app['websockets']):
             await ws.send_str(line)
 
@@ -58,8 +69,13 @@ async def start_background_tasks(app):
     app['system_process'] = asyncio.create_task(system_process(app))
 
 async def cleanup_background_tasks(app):
+    print('cleaning up', flush=True)
     app['system_process'].cancel()
-    await app['system_process']
+    # await app['system_process']
+
+async def cancel_background_tasks(app):
+    app['system_process'].cancel()
+    print("cancel_background_tasks")
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
@@ -84,9 +100,9 @@ if __name__ == "__main__":
     app['command'] = command
     app['args'] = args
     app['websockets'] = weakref.WeakSet()
-    app['process'] = None
     app.on_startup.append(start_background_tasks)
     app.on_cleanup.append(cleanup_background_tasks)
+    app.on_shutdown.append(cancel_background_tasks)
     app.add_routes([
         web.get('/', websocket_handler)
     ])
